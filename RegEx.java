beef882b27d2703069927145f33a2bbe963ca49d
Mayoor <mayoordoiphode@gmail.com>
2022-04-27 00:25:21 +0530

    Added: EmailID pattern

--- RegEx.java
+++ RegEx.java
@@ -5,34 +5,54 @@ import java.util.regex.Pattern;
 
 public class RegEx {
     //    First name pattern
-    public void checkFirstName() {
+    public static void checkFirstName() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first name: ");
         String firstName = sc.nextLine();
         if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
-            System.out.println( "Valid entry..");
+            System.out.println( "Valid entry.\n");
         } else {
-            System.err.println("Invalid entry!\n-First letter should be capital\n-First name should contain at least 3 letters\n");
+            System.err.println("Invalid entry!\n-First letter should be capital,other letters should be small" +
+                    "\n-First name should contain at least 3 letters\n");
+            checkFirstName();
         }
     }
 
     //    Last name pattern
-    public void checkLastName() {
+    public static void checkLastName() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Last name: ");
         String lastName = sc.nextLine();
         if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName)) {
-            System.out.println("valid entry..");
+            System.out.println("valid entry.\n");
         } else {
-            System.out.println("Invalid entry!");
-            System.err.println("-First letter should be capital\n-Last name should contain at least 3 letters");
+            System.err.println("Invalid entry!\n-First letter should be capital" +
+                    "\n-Last name should contain at least 3 letters\n");
+            checkLastName();
+        }
+    }
+
+    //    EmailID pattern
+    public static void checkEmail() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Email : ");
+        String email = sc.nextLine();
+        if(Pattern.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*[@][a-z]+[.][a-z]{2,3}([.][a-z]{2})?$",email)){
+            System.out.println("Valid entry.");
+        }
+        else {
+            System.err.println("Invalid entry!\n-user name should have at least 3 letters" +
+                    "\n-It should contain . after company name, company domain" +
+                    "\n-Company name should be 2 letters" +
+                    "\n-Company domain should be 2 letters" );
+            checkEmail();
         }
     }
 
     public static void main(String[] args) {
 
-        RegEx userReg = new RegEx();
-        userReg.checkFirstName();
-        userReg.checkLastName();
+        checkFirstName();
+        checkLastName();
+        checkEmail();
     }
 }
