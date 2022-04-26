3242bee51308f4aa32c1788fd4bb6df9d7709aea
Mayoor <mayoordoiphode@gmail.com>
2022-04-26 23:47:05 +0530

    Added: First and last name pattern

--- RegEx.java
+++ RegEx.java
@@ -1,22 +1,38 @@
 package com.bridgelabz.assignment19;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class RegEx {
-//    First name pattern
-    public static boolean FirstName(String firstName){
-        return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
-    }
-    public static void main(String[] args) {
-        System.out.println("User Name: \nEnter the first name: ");
+    //    First name pattern
+    public void checkFirstName() {
         Scanner sc = new Scanner(System.in);
-        boolean result = FirstName(sc.next());
-        if (result) {
-            System.out.println("User name is valid, please proceed..");
+        System.out.println("Enter the first name: ");
+        String firstName = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
+            System.out.println( "Valid entry..");
+        } else {
+            System.err.println("Invalid entry!\n-First letter should be capital\n-First name should contain at least 3 letters\n");
         }
-        else {
-            System.out.println("Invalid first name!");
-            System.err.println("-First letter should be capital\n-Name should contain at least 3 letters");
+    }
+
+    //    Last name pattern
+    public void checkLastName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Last name: ");
+        String lastName = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName)) {
+            System.out.println("valid entry..");
+        } else {
+            System.out.println("Invalid entry!");
+            System.err.println("-First letter should be capital\n-Last name should contain at least 3 letters");
         }
+    }
+
+    public static void main(String[] args) {
+
+        RegEx userReg = new RegEx();
+        userReg.checkFirstName();
+        userReg.checkLastName();
     }
 }
